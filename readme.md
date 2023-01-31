1b9850fcea82e8e6fc462496dd1786302f8ba2d7
Максим Водянов <max.post@bk.ru>
2023-01-31 10:30:17 +0300

    init webpack

---
+++ readme.md
@@ -0,0 +1,13 @@
+## About 
+This template project contains a lot of different modern frontend technologies. 
+Such as 
+- webpack 5 (config)
+- typescript
+
+Inspired by UbliTV
+
+# Requirements
+Node.js - 17.4.0
+npm  - 8.5.1
+
+#
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// import path from "path";
+// import webpack from 'webpack';
+// import HTMLWebpackPlugin from "html-webpack-plugin";
+const path =  require("path");
+const webpack =  require('webpack');
+const HTMLWebpackPlugin =  require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new webpack.ProgressPlugin(),
+],
+};
