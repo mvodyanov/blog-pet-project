12165c870f6d2e91bd647ffa5fd50a5cb32801a4
Максим Водянов <max.post@bk.ru>
2023-02-04 14:28:38 +0300

    init react browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
 import "./index.scss";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 export const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path={"/"} element={<MainPage/>} />
+        <Route path={"/about"} element={<AboutPage/>} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "../../components/Counter";
+
+const AboutPage = () => {
+  return (
+    <div>
+      About page
+      <Counter />
+    </div>
+  );
+};
+
+export default AboutPage;
