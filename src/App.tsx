0d66a33ce2a5cbfd58872fff517314a2ac8b5354
Максим Водянов <max.post@bk.ru>
2023-02-04 14:34:48 +0300

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+
 import "./index.scss";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 
 export const App = () => {
   return (
     <div className="app">
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
-      <Routes>
-        <Route path={"/"} element={<MainPage/>} />
-        <Route path={"/about"} element={<AboutPage/>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
