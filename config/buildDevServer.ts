0dded361227f1cb6369de596828560ffd7ea2a00
Максим Водянов <max.post@bk.ru>
2023-02-10 12:37:00 +0300

    webpack hot module replacement

--- config/buildDevServer.ts
+++ config/buildDevServer.ts
@@ -4,7 +4,8 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port,
-        historyApiFallback: true
+        historyApiFallback: true,
+        hot: true
         // open: true,
     }
 }
