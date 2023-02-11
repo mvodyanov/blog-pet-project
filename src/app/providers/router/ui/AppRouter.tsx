16997f924831ececf582967436d8a51982c73b09
Максим Водянов <max.post@bk.ru>
2023-02-11 17:57:13 +0300

    not found and page loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,18 +2,19 @@ import { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widget/PageLoader/PageLoader';
 
 export const AppRouter = () => {
     const { t } = useTranslation();
     return (
-        <Suspense fallback={<div>{t('Загрузка')}</div>}>
+        <Suspense fallback={<PageLoader />}>
             <Routes>
                 {Object.values(routeConfig).map(({ path, element }) => (
                     <Route
                         key={path}
                         path={path}
                         element={(
-                            <Suspense fallback={<div>{t('Загрузка')}</div>}>
+                            <Suspense fallback={<PageLoader />}>
                                 <div className="page-wrapper">{element}</div>
                             </Suspense>
                         )}
