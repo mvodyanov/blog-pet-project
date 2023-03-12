53d988c93f055c1c220e5b32ce3b62e4708f85e8
m.vodianov <m.vodianov@z-it.ru>
2023-03-12 16:12:30 +0300

    43 Profile tests

--- src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.ts
@@ -7,7 +7,7 @@ import { validateProfileData } from '../validateProfileData/validateProfileData'
 export const updateProfileData = createAsyncThunk<
     Profile,
     void,
-    ThunkConfig<ValidateProfileError[] >
+    ThunkConfig<ValidateProfileError[]>
     >(
         'profile/updateProfileData',
         async (_, thunkApi) => {
@@ -23,6 +23,10 @@ export const updateProfileData = createAsyncThunk<
 
             try {
                 const response = await extra.api.put<Profile>('/profile', formData);
+
+                if (!response.data) {
+                    throw new Error();
+                }
 
                 return response.data;
             } catch (e) {
