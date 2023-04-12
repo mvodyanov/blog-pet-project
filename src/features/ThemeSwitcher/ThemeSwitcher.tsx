d53e665c0e93672f4ec89049c5a1a9407f42b167
m.vodianov <m.vodianov@z-it.ru>
2023-04-12 18:54:26 +0300

    64: fsd refactoring

--- src/features/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,10 +1,9 @@
-import { classNames } from 'shared/lib/classNames/classNames';
 import { FC } from 'react';
 import { useTheme, Theme } from 'app/providers/ThemeProvider';
-
+import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import { Button, ButtonTheme } from '../Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
   className?: string;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { memo, useMemo, useState } from 'react';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
+import { ThemeSwitcher } from 'features/ThemeSwitcher/ThemeSwitcher';
 import { useSelector } from 'react-redux';
 import { getSidebarItems } from '../../model/selectors/getSidebarItems';
 import cls from './Sidebar.module.scss';
